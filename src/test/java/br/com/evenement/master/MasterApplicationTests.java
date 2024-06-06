a370ec9ece06efb09a4523bf9d9db27b3857b396
DanielSanoli <daniel.sanoli28@gmail.com>
2024-06-05 22:09:02 -0300

    configs classe test

--- src/test/java/br/com/evenement/master/MasterApplicationTests.java
+++ src/test/java/br/com/evenement/master/MasterApplicationTests.java
@@ -3,7 +3,9 @@ package br.com.evenement.master;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import br.com.evenement.api.MasterApplication;
+
+@SpringBootTest(classes = MasterApplication.class)
 class MasterApplicationTests {
 
 	@Test
