cee21f31d8290fea7332f0a0d0c89ef9d27b417c
DanielSanoli <daniel.sanoli28@gmail.com>
2024-06-12 14:30:42 -0300

    mapeando e ajustando o cadastro de pessoas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HttpClientModule } from '@angular/common/http';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'cadastrar', component: CadastroComponent}
 ];
 
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes), HttpClientModule],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
