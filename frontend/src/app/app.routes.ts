3f9d839fb6f8a4fd0bde5c545f1ab7e36c883632
DanielSanoli <daniel.sanoli28@gmail.com>
2024-06-13 20:02:08 -0300

    criando component login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'cadastrar', component: CadastroComponent}
+    { path: 'cadastrar', component: CadastroComponent },
+    { path: 'login', component: LoginComponent},
 ];
 
 
