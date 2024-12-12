f3159b39aa179a8aa258155c9d9d22f810e89d69
danielsanoli <daniel.sanoli28@gmail.com>
2024-12-12 19:09:13 -0300

    inserindo configuração do HttpClient

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appRoutingProviders } from './app/app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [appRoutingProviders]
+  providers: [
+    appRoutingProviders,
+    provideHttpClient(withFetch())
+  ]
 })
   .catch((err) => console.error(err));
